b0e1f2d7dbbd48706de5dcd523df3e1765f30eb7
Changsu Im <imcxsu@gmail.com>
2022-04-17 03:40:01 +0900

    Change default port number

--- 1.4.200/build.sh
+++ 1.4.200/build.sh
@@ -6,12 +6,16 @@ IMAGE_TAG=1.4.200
 
 REMOTE=$IMAGE_REPOSITORY/$IMAGE_NAME:$IMAGE_TAG
 
+# docker build --force-rm=true --no-cache=true --tag=markruler/h2 .
 sudo docker build \
   --force-rm=true \
   --no-cache=true \
   --tag=$IMAGE_REPOSITORY/$IMAGE_NAME .
 
+# docker tag markruler/h2 markruler/h2:1.4.200
 sudo docker tag $IMAGE_REPOSITORY/$IMAGE_NAME $REMOTE
 
+# docker push markruler/h2
 sudo docker push $IMAGE_REPOSITORY/$IMAGE_NAME # latest
+# docker push markruler/h2:1.4.200
 sudo docker push $REMOTE
